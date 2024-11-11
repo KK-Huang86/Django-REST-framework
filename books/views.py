4a89e0ae436dcabb14bef3a2d696289263e9f9a3
Kk <iankk0806@gmail.com>
2024-11-11 22:00:41 +0800

    feat: add Authentications for api

--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 from books.models import Book
@@ -9,3 +10,4 @@ from books.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = (IsAuthenticated,)
--- core/urls.py
+++ core/urls.py
@@ -25,4 +25,8 @@ router = DefaultRouter()
 router.register(r"book", views.BookViewSet)
 
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/", include(router.urls))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+]
